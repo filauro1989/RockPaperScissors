cba3d8473ccddd655a0944697ecc9fb320e92cad
Francesco Filauro <francesco.filauro@isendu.com>
2023-09-14 12:45:58 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/filauro1989/',
+  base: '.',
   plugins: [
     svelte(),
     VitePWA({
